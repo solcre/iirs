58701c86124e2c8c6ae605d9d3fd94d2b92f1b8d
carlux <carlux@a6dac886-a6da-11de-bf1c-ef771b8a44ac>
2009-09-24 09:08:12 +0000

    nuevos cambios, arregle un bugsito y agregue un nuevo metodo para eliminar caracteres raros, tildes, eñes, pasaje a minusculas,etc.

--- index.php
+++ index.php
@@ -6,18 +6,30 @@
   </head>
   <body>
     <?php
+
+
+
 	 //Probando eliminar stopwords
          
-         $cadena="cortando las palabras porque son las palabras muchisimisimas palabrotas a cortar porque las palabras son asi";
+         $cadena="Por poema se entendía antiguamente a cualquier composición literaria, ya que la palabra viene del verbo griego poiesis, que significa creación con la palabra. El poema es así, cualquier lectura o trabajo escrito con intención artística. Por lo cual se hablaba de poesía épica (luego narrativa); lírica (o destinada al canto del arpa manual de cinco cuerdas conocida como lira); y poesía dramática o destinada a la representación escénica. Y por literario hay que entender el lenguaje sometido a un tratamiento de estilización para intensificar su belleza que lo convierte en objeto de arte. El poema es el lugar de encuentro entre la poesía y el hombre. Dicho por Octavio Paz, en su obra El arco y la lira no todo texto construido bajo las leyes del metro es poesía.";
          echo "<b>Cadena inicial: </b>".$cadena;
          echo "<br/>";
          echo "<br/>";
-         $canonica=quitar_comunes($cadena);
-         echo "<b>Cadena final: </b>".$canonica;
+
+
+         $canonica=preparar_texto($cadena);
+	 //eliminamos stopwords
+	 $canonica=quitar_comunes($canonica);
+         echo "<b>Se remueven stopwords, caracteres irrelevantes y se cambian caracteres: </b>".$canonica;
          echo "<br/>";
 
+
 	 //probando generar relevancias en indice
-	 relevancias($cadena);
+	 relevancias($canonica);
+         echo "<br/>";
+         echo "<br/>";
+
+
          
     ?>  
   </body>
