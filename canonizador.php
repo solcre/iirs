2e6ba1e86ff987bdbf47df4cbc5e6d4471d96918
carlux <carlux@a6dac886-a6da-11de-bf1c-ef771b8a44ac>
2009-09-24 09:08:03 +0000

    nuevos cambios, arregle un bugsito y agregue un nuevo metodo para eliminar caracteres raros, tildes, eñes, pasaje a minusculas,etc.

--- canonizador.php
+++ canonizador.php
@@ -5,7 +5,7 @@
  * Remueve stopwords
  */
 function quitar_comunes($cadena){
-    $comunes = array('la','el','él','de','la','las','que','a','y','o','ya','en','los','las','le','lo','un','una','se','es','no','si','por','con','del','al','su','sus');
+    $comunes = array('por','la',' a','el','de','las','que','y','o','ya','en','los','las','le','lo','un','una','se','es','no','si','con','del','al','su','sus','asi');
     return preg_replace('/\b('.implode('|',$comunes).')\b/','',$cadena);
 }
  
@@ -13,14 +13,12 @@ function quitar_comunes($cadena){
  * Calcula relevancias en una cadena dada generando un indice.
  */
 function relevancias($cadena){
-	echo "<h1>Palabras extraidas</h1>";
-	$palabrasConRepetidos = explode(" ", $cadena);
-	foreach($palabrasConRepetidos as $palabra){
-		echo $palabra."<br/>";
-	} 
-	
-	$palabrasSinRepetidos=array_unique($palabrasConRepetidos);
 
+	$cadena=preparar_texto($cadena);
+	$palabrasConRepetidos = explode(" ", $cadena);
+	$palabrasConRepetidos = array_diff($palabrasConRepetidos, array(""));
+	$palabrasSinRepetidos=array_unique($palabrasConRepetidos);
+	
 	//inicializo puntajes	
 	foreach ($palabrasSinRepetidos as $palabra){
 		$palabrasConPuntajes[$palabra]=0;
@@ -47,6 +45,10 @@ function relevancias($cadena){
 	$posicion=0;
 	foreach($palabrasSinRepetidos as $palabraSR){
 		$palabrasConPuntajes[$palabraSR]=(($cantPalabras-$posicion)/$cantPalabras)*$palabrasConPuntajes[$palabraSR];
+
+		//las lineas comentadas a continuación pertenecen al puntaje otorgado dando relevancia a las últimas palabras y no a la sprimeras.
+		//$posicion=strrpos($cadena, $palabraSR)/10;//la diferencia de el algoritmo de puntajes por derecha es que da la posición según caracteres y no según palabras
+		//$palabrasConPuntajes[$palabraSR]=((1+$posicion)/$cantPalabras)*$palabrasConPuntajes[$palabraSR];
 		$posicion++;
 	}
 
@@ -73,7 +75,24 @@ function comparacion_puntajes($a, $b) {
 }
 
  
-      
+function preparar_texto($cadena)
+{
+	$cadena=strtolower($cadena);
+	$aCambiar = array('Ñ','ñ','á','à','â','ã','ª','Á',
+			'À','Â','Ã', 'é','è','ê','É','È',
+			'Ê','í','ì','î','Í','Ì','Î','ò',
+			'ó','ô','õ','º','Ó','Ò','Ô','Õ',
+			'ú','ù','û','Ú','Ù','Û','ç','Ç');
+
+	$cambiarPor = array('N','n','a','a','a','a','a','A',
+			'A','A','A','e','e','e','E','E',
+			'E','i','i','i','I','I','I','o',
+			'o','o','o','o','O','O','O','O',
+			'u','u','u','U','U','U','c','C');
+    $cadena = trim(str_replace($aCambiar, $cambiarPor, $cadena));
+    $cadena = trim(str_replace('  ', ' ', $cadena));
+    return ereg_replace('[^A-Za-z0-9\_\-]', ' ', $cadena);
+}       
       
       
 ?>
